b29285679269e1e207bfdd5f79fbc732cc897924
djescalante <jde.sistemas@gmail.com>
2021-12-09 21:50:27 -0500

    .

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 class HomePageTem extends StatelessWidget {
-  const HomePageTem({Key? key}) : super(key: key);
+  //las propiedades dentro de un statelesswirdget deben ser fina
+  static const opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis'];
 
+  const HomePageTem({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes+'),
+        title: const Text('Componentes Temp'),
       ),
-      body: _lista(),
+      body: ListView(children: _crearItems()),
     );
   }
 
-  Widget _lista() {
-    return ListView();
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[]; // ya no se susa = List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(const Divider());
+      //lista.add(tempWidget);
+      //lista.add(const Divider());
+    }
+
+    return lista;
   }
 }
